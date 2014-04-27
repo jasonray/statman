784f4f728ca2eead831097c87ab632f3fa68a2a2
Jason Ray <jason.ray@agilex.com>
2014-04-27 10:48:27 -0400

    clean up unit test

--- test/gauage-register.js
+++ test/gauage-register.js
@@ -1,14 +1,17 @@
 var registry = require('../lib/registry');
+var gaugelib = require('../lib/gauge');
 
 exports.activelyRegisterGauage = function(test) {
-	var gauge = registry.gauges().register('metric-name');
+	var gauge = new gaugelib.Gauge('metric-name');
+	registry.gauges().register(gauge);
 	test.equal('metric-name', gauge.name());
 	test.done();
 };
 
 exports.retrieveGauage = function(test) {
-	registry.gauges().register('metric-name');
-	var gauge = registry.gauges('metric-name');
+	var gauge = new gaugelib.Gauge('metric-name');
+	registry.gauges().register(gauge);
+	var retrievedGauge = registry.gauges('metric-name');
 	test.equal('metric-name', gauge.name());
 	test.done();
 };
