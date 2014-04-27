d71456e9692d7c46c35686633b8f66d7aa8a292c
Jason Ray <jason.ray@agilex.com>
2014-04-27 10:13:31 -0400

    allow for registration of gauge

---
+++ test/gauage-register.js
@@ -0,0 +1,10 @@
+var registry = require('../lib/registry');
+
+exports.activelyRegisterGauage = function(test) {
+	// var gauage = registry.guages.register('metric-name');
+	var gauges = registry.gauges();
+	var gauge = gauges.register('metric-name');
+	test.equal('metric-name', gauge.name());
+	test.done();
+};
+
