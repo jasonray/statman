6e4c514639c51961bdb6f16d9ce67060e6528348
Jason Ray <jason.ray@agilex.com>
2014-04-27 10:16:41 -0400

    clean up of test method formatting

--- test/gauage-register.js
+++ test/gauage-register.js
@@ -1,9 +1,7 @@
 var registry = require('../lib/registry');
 
 exports.activelyRegisterGauage = function(test) {
-	// var gauage = registry.guages.register('metric-name');
-	var gauges = registry.gauges();
-	var gauge = gauges.register('metric-name');
+	var gauge = registry.gauges().register('metric-name');
 	test.equal('metric-name', gauge.name());
 	test.done();
 };
