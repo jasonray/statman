227e08b6e804b76cfa92c41ec004814d943fbf97
Jason Ray <jason.ray@agilex.com>
2014-11-16 14:05:34 -0500

    add test for stopwatch delta

--- test/stopwatch-test.js
+++ test/stopwatch-test.js
@@ -4,28 +4,36 @@
 var Stopwatch = require('../lib/Stopwatch');
 
 exports.initStopwatch = function(test) {
-	var stopwatch = new Stopwatch('mystopwatch');
-	test.equal(stopwatch.name, 'mystopwatch');
-	test.done();
+    var stopwatch = new Stopwatch('mystopwatch');
+    test.equal(stopwatch.name, 'mystopwatch');
+    test.done();
 };
 
 exports.initStopwatchWithoutName = function(test) {
-	var stopwatch = new Stopwatch();
-	test.ok(stopwatch.name !== undefined);
-	test.done();
+    var stopwatch = new Stopwatch();
+    test.ok(stopwatch.name !== undefined);
+    test.done();
 };
 
 exports.startAndReadStopwatch = function(test) {
-	var stopwatch = new Stopwatch('mystopwatch');
-	console.log('starting stopwatch');
-	stopwatch.start();
-	console.log('started stopwatch');
-	setTimeout(function() {
-		console.log('finishing stopwatch');
-		var delta = stopwatch.read();
-		// console.log('read stopwatch [%s]', delta);
-		// test.equal(delta, 1000);
-		test.done();
-		console.log('done!');
-	}, 1000);
+    var stopwatch = new Stopwatch('mystopwatch');
+    console.log('starting stopwatch');
+    stopwatch.start();
+    console.log('started stopwatch');
+    setTimeout(function() {
+        console.log('finishing stopwatch');
+        var delta = stopwatch.read();
+        console.log('read stopwatch [%s]', delta);
+        verifyDelta(test, 1000, delta, 10);
+        test.done();
+        console.log('done!');
+    }, 1000);
 };
+
+function verifyDelta(test, expected, actual, acceptedVariance) {
+    var lowerThreshold = expected - acceptedVariance;
+    var upperThreshold = expected + acceptedVariance;
+    var message = "Expected " + expected + " ± " + acceptedVariance + ", but was " + actual + ".";
+    console.log(message);
+    test.ok((actual >= lowerThreshold) && (actual <= upperThreshold), message);
+}
