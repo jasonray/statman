bb5bd368684ad9c1ea67b42961d1ffbc99e09fb4
Jason Ray <jason.ray@agilex.com>
2014-11-16 14:10:33 -0500

    cleanup stopwatch test

--- test/stopwatch-test.js
+++ test/stopwatch-test.js
@@ -18,31 +18,21 @@ exports.initStopwatchWithoutName = function(test) {
 
 exports.startAndReadStopwatch1000 = function(test) {
     var stopwatch = new Stopwatch('mystopwatch');
-    console.log('starting stopwatch');
     stopwatch.start();
-    console.log('started stopwatch');
     setTimeout(function() {
-        console.log('finishing stopwatch');
         var delta = stopwatch.read();
-        console.log('read stopwatch [%s]', delta);
         verifyDelta(test, 1000, delta, defaultPrecision);
         test.done();
-        console.log('done!');
     }, 1000);
 };
 
 exports.startAndReadStopwatch10 = function(test) {
     var stopwatch = new Stopwatch('mystopwatch');
-    console.log('starting stopwatch');
     stopwatch.start();
-    console.log('started stopwatch');
     setTimeout(function() {
-        console.log('finishing stopwatch');
         var delta = stopwatch.read();
-        console.log('read stopwatch [%s]', delta);
         verifyDelta(test, 10, delta, defaultPrecision);
         test.done();
-        console.log('done!');
     }, 10);
 };
 
@@ -50,6 +40,5 @@ function verifyDelta(test, expected, actual, acceptedVariance) {
     var lowerThreshold = expected - acceptedVariance;
     var upperThreshold = expected + acceptedVariance;
     var message = "Expected " + expected + " ± " + acceptedVariance + ", but was " + actual + ".";
-    console.log(message);
     test.ok((actual >= lowerThreshold) && (actual <= upperThreshold), message);
 }
