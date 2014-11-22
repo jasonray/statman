6663c24f1d9bc284fbfbb8f34af9afa00abb3fab
Jason Ray <jason.ray@agilex.com>
2014-11-22 16:34:02 -0500

    add test for stopping stop watch twice

--- test/stopwatch-test.js
+++ test/stopwatch-test.js
@@ -103,6 +103,26 @@ exports.stopWithoutStart = function(test) {
     test.done();
 };
 
+exports.utilizeStopTwice = function(test) {
+    var stopwatch = new Stopwatch('b');
+    stopwatch.start();
+    setTimeout(function() {
+        stopwatch.stop();
+
+        setTimeout(function() {
+            stopwatch.stop();
+            setTimeout(function() {
+                var delta = stopwatch.read();
+                verifyDelta(test, 2000, delta, defaultPrecision);
+                test.done();
+
+            }, 1000);
+
+        }, 1000);
+
+    }, 1000);
+};
+
 function verifyDelta(test, expected, actual, acceptedVariance) {
     var lowerThreshold = expected - acceptedVariance;
     var upperThreshold = expected + acceptedVariance;
