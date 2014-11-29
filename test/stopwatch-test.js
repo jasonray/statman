29d6c4d68f9910fff9d91eb7d7f20ba93ab1f437
Jason Ray <jason.ray@agilex.com>
2014-11-28 22:21:27 -0500

    formatting

--- test/stopwatch-test.js
+++ test/stopwatch-test.js
@@ -37,33 +37,6 @@ exports.utilizeStop = function(test) {
     }, 1000);
 };
 
-// exports.stopWithoutStart = function(test) {
-//     var stopwatch = new Stopwatch('a');
-//     stopwatch.stop();
-//     test.ok(isNaN(stopwatch.read()));
-//     test.done();
-// };
-
-// exports.utilizeStopTwice = function(test) {
-//     var stopwatch = new Stopwatch('b');
-//     stopwatch.start();
-//     setTimeout(function() {
-//         stopwatch.stop();
-
-//         setTimeout(function() {
-//             stopwatch.stop();
-//             setTimeout(function() {
-//                 var delta = stopwatch.read();
-//                 verifyDelta(test, 2000, delta, defaultPrecision);
-//                 test.done();
-
-//             }, 1000);
-
-//         }, 1000);
-
-//     }, 1000);
-// };
-
 function verifyDelta(test, expected, actual, acceptedVariance) {
     var lowerThreshold = expected - acceptedVariance;
     var upperThreshold = expected + acceptedVariance;
