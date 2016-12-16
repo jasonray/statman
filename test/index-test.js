4d387b81ddd85081d7f442e190ec20f33cc76ee5
Jason Ray <jasonray@jayray.net>
2016-12-16 07:12:53 -0500

    move test to mocha

--- test/index-test.js
+++ test/index-test.js
@@ -1,20 +1,20 @@
-// /*jslint node: true */
-// "use strict";
-//
-// var metrics = require('../lib/metrics');
-//
-// exports.initGauge = function(test) {
-// 	var gauge = new metrics.Gauge('metric-name');
-// 	gauge.increment();
-// 	test.equal(1, gauge.value());
-// 	test.done();
-// };
-//
-// exports.registerGauge = function(test) {
-// 	var gauge = new metrics.Gauge('metric-name');
-// 	metrics.register(gauge);
-// 	gauge.increment();
-// 	test.equal(1, gauge.value());
-// 	test.done();
-// };
-//
+/*jslint node: true */
+"use strict";
+
+var metrics = require('../lib/metrics');
+
+exports.initGauge = function(test) {
+	var gauge = new metrics.Gauge('metric-name');
+	gauge.increment();
+	test.equal(1, gauge.value());
+	test.done();
+};
+
+exports.registerGauge = function(test) {
+	var gauge = new metrics.Gauge('metric-name');
+	metrics.register(gauge);
+	gauge.increment();
+	test.equal(1, gauge.value());
+	test.done();
+};
+
