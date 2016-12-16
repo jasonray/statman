75fae093ed4a5177f9d3c7eb722fb06ea253cd52
Jason Ray <jasonray@jayray.net>
2016-12-16 07:11:31 -0500

    moving to mocha, disabling non-mocha tests for now

--- test/gauge-registry-test.js
+++ test/gauge-registry-test.js
@@ -1,44 +1,44 @@
-var metrics = require('../lib/metrics');
-
-exports.activelyRegisterGauage = function(test) {
-	var gauge = new metrics.Gauge('metric-name');
-	metrics.register(gauge);
-	test.equal('metric-name', gauge.name());
-	test.done();
-};
-
-exports.retrieveGauage = function(test) {
-	var gauge = new metrics.Gauge('metric-name');
-	metrics.register(gauge);
-	var retrievedGauge = metrics.registry('metric-name');
-	test.equal('metric-name', gauge.name());
-	test.done();
-};
-
-exports.modifyValueOfGauge = function(test) {
-	var gauge = new metrics.Gauge('a');
-	metrics.register(gauge);
-
-	metrics.registry('a').set(5);
-	metrics.registry('a').increment();
-	metrics.registry('a').increment();
-	metrics.registry('a').decrement(3);
-	metrics.registry('a').increment(7);
-
-	test.equal(11, metrics.registry('a').value());
-	test.done();
-};
-
-exports.autoregister = function(test) {
-	var gauge = metrics.gauges('auto');
-
-	test.equal('auto', gauge.name());
-	gauge.increment();
-	test.equal(1, gauge.value());
-
-	gauge2 = metrics.gauges('auto');
-	test.equal('auto', gauge2.name());
-	test.equal(1, gauge2.value());
-
-	test.done();
-};
+// var metrics = require('../lib/metrics');
+//
+// exports.activelyRegisterGauage = function(test) {
+// 	var gauge = new metrics.Gauge('metric-name');
+// 	metrics.register(gauge);
+// 	test.equal('metric-name', gauge.name());
+// 	test.done();
+// };
+//
+// exports.retrieveGauage = function(test) {
+// 	var gauge = new metrics.Gauge('metric-name');
+// 	metrics.register(gauge);
+// 	var retrievedGauge = metrics.registry('metric-name');
+// 	test.equal('metric-name', gauge.name());
+// 	test.done();
+// };
+//
+// exports.modifyValueOfGauge = function(test) {
+// 	var gauge = new metrics.Gauge('a');
+// 	metrics.register(gauge);
+//
+// 	metrics.registry('a').set(5);
+// 	metrics.registry('a').increment();
+// 	metrics.registry('a').increment();
+// 	metrics.registry('a').decrement(3);
+// 	metrics.registry('a').increment(7);
+//
+// 	test.equal(11, metrics.registry('a').value());
+// 	test.done();
+// };
+//
+// exports.autoregister = function(test) {
+// 	var gauge = metrics.gauges('auto');
+//
+// 	test.equal('auto', gauge.name());
+// 	gauge.increment();
+// 	test.equal(1, gauge.value());
+//
+// 	gauge2 = metrics.gauges('auto');
+// 	test.equal('auto', gauge2.name());
+// 	test.equal(1, gauge2.value());
+//
+// 	test.done();
+// };
--- test/stopwatch-test.js
+++ test/stopwatch-test.js
@@ -1,45 +1,45 @@
-/*jslint node: true */
-"use strict";
-
-var Stopwatch = require('../lib/metrics').Stopwatch;
-var defaultPrecision = 5;
-
-exports.startAndReadStopwatch1000 = function(test) {
-    var stopwatch = new Stopwatch();
-    stopwatch.start();
-    setTimeout(function() {
-        var delta = stopwatch.read();
-        verifyDelta(test, 1000, delta, defaultPrecision);
-        test.done();
-    }, 1000);
-};
-
-exports.autostart = function(test) {
-    var stopwatch = new Stopwatch(true);
-    setTimeout(function() {
-        var delta = stopwatch.read();
-        verifyDelta(test, 1000, delta, defaultPrecision);
-        test.done();
-    }, 1000);
-};
-
-exports.utilizeStop = function(test) {
-    var stopwatch = new Stopwatch();
-    stopwatch.start();
-    setTimeout(function() {
-        stopwatch.stop();
-
-        setTimeout(function() {
-            var delta = stopwatch.read();
-            verifyDelta(test, 1000, delta, defaultPrecision);
-            test.done();
-        }, 1000);
-    }, 1000);
-};
-
-function verifyDelta(test, expected, actual, acceptedVariance) {
-    var lowerThreshold = expected - acceptedVariance;
-    var upperThreshold = expected + acceptedVariance;
-    var message = "Expected " + expected + " ± " + acceptedVariance + ", was " + actual + ".";
-    test.ok((actual >= lowerThreshold) && (actual <= upperThreshold), message);
-}
+// /*jslint node: true */
+// "use strict";
+//
+// var Stopwatch = require('../lib/metrics').Stopwatch;
+// var defaultPrecision = 5;
+//
+// exports.startAndReadStopwatch1000 = function(test) {
+//     var stopwatch = new Stopwatch();
+//     stopwatch.start();
+//     setTimeout(function() {
+//         var delta = stopwatch.read();
+//         verifyDelta(test, 1000, delta, defaultPrecision);
+//         test.done();
+//     }, 1000);
+// };
+//
+// exports.autostart = function(test) {
+//     var stopwatch = new Stopwatch(true);
+//     setTimeout(function() {
+//         var delta = stopwatch.read();
+//         verifyDelta(test, 1000, delta, defaultPrecision);
+//         test.done();
+//     }, 1000);
+// };
+//
+// exports.utilizeStop = function(test) {
+//     var stopwatch = new Stopwatch();
+//     stopwatch.start();
+//     setTimeout(function() {
+//         stopwatch.stop();
+//
+//         setTimeout(function() {
+//             var delta = stopwatch.read();
+//             verifyDelta(test, 1000, delta, defaultPrecision);
+//             test.done();
+//         }, 1000);
+//     }, 1000);
+// };
+//
+// function verifyDelta(test, expected, actual, acceptedVariance) {
+//     var lowerThreshold = expected - acceptedVariance;
+//     var upperThreshold = expected + acceptedVariance;
+//     var message = "Expected " + expected + " ± " + acceptedVariance + ", was " + actual + ".";
+//     test.ok((actual >= lowerThreshold) && (actual <= upperThreshold), message);
+// }
