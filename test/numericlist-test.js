9395c1a2aebf77d92bd3537694fb1f0cec323e6d
Jason Ray <jasonray@jayray.net>
2016-12-17 13:36:31 -0500

    cleanup

--- test/numericlist-test.js
+++ test/numericlist-test.js
@@ -29,25 +29,25 @@ describe('numeric list', function () {
         list.push(0);
         list.push(0);
         list.push(0);
-        assert.equal(list.size(), 5);
+        assertCloseEnough(list.size(), 5);
     });
 
     it('given an empty list, sum should return 0', function () {
         var list = new NumericList();
-        assert.equal(list.sum(), 0);
+        assertCloseEnough(list.sum(), 0);
     });
 
     it('given a list of one integer number, sum should return the number', function () {
         var list = new NumericList();
         list.push(5);
-        assert.equal(list.sum(), 5);
+        assertCloseEnough(list.sum(), 5);
     });
 
     it('given a list of two integer numbers, sum should return the sum of the numbers', function () {
         var list = new NumericList();
         list.push(2);
         list.push(4);
-        assert.equal(list.sum(), 6);
+        assertCloseEnough(list.sum(), 6);
     });
 
     it('given a list of five integer numbers, sum should return the sum of the numbers', function () {
@@ -57,13 +57,13 @@ describe('numeric list', function () {
         list.push(3);
         list.push(4);
         list.push(5);
-        assert.equal(list.sum(), 15);
+        assertCloseEnough(list.sum(), 15);
     });
 
     it('given a list of one decimal number, sum should return the number', function () {
         var list = new NumericList();
         list.push(5.5);
-        assert.equal(list.sum(), 5.5);
+        assertCloseEnough(list.sum(), 5.5);
     });
 
     it('given a list of two decimal numbers, sum should return the sum of the numbers', function () {
@@ -80,25 +80,25 @@ describe('numeric list', function () {
         list.push(3.3);
         list.push(4.4);
         list.push(5.5);
-        assert.equal(list.sum(), 16.5);
+        assertCloseEnough(list.sum(), 16.5);
     });
 
     it('given an empty list, average should return 0', function () {
         var list = new NumericList();
-        assert.equal(list.average(), 0);
+        assertCloseEnough(list.average(), 0);
     });
 
     it('given a list of one integer number, average should return the number', function () {
         var list = new NumericList();
         list.push(5);
-        assert.equal(list.average(), 5);
+        assertCloseEnough(list.average(), 5);
     });
 
     it('given a list of two integer numbers, sum should return the average of the numbers', function () {
         var list = new NumericList();
         list.push(4);
         list.push(6);
-        assert.equal(list.average(), 5);
+        assertCloseEnough(list.average(), 5);
     });
 
     it('given a list of five integer numbers, sum should return the average of the numbers', function () {
@@ -108,7 +108,7 @@ describe('numeric list', function () {
         list.push(3);
         list.push(4);
         list.push(5);
-        assert.equal(list.average(), 3);
+        assertCloseEnough(list.average(), 3);
     });
 
     it('given a list of one decimal number, average should return the number', function () {
@@ -131,14 +131,13 @@ describe('numeric list', function () {
         list.push(3.3);
         list.push(4.4);
         list.push(5.5);
-        // assert.equal(list.average(), 3.3);
         assertCloseEnough(list.average(), 3.3)
     });
 
-    it('pushing a list of 10,000 numbers and calculating sum/ave should run within milliseconds', function () {
+    it('pushing a list of 100,000 numbers and calculating sum/ave should run within milliseconds', function () {
         this.timeout(1000);
 
-        var testsize = 1000000;
+        var testsize = 100000;
         var list = new NumericList();
 
         for (var i = 1; i <= testsize; i++) {
@@ -155,9 +154,9 @@ describe('numeric list', function () {
 
         var expected = getExpected(testsize);
 
-        assert.equal(list.size(), expected.size, "size");
-        assert.equal(list.sum(), expected.sum, "sum");
-        assert.equal(list.average(), expected.average, "ave");
+        assertCloseEnough(list.size(), expected.size, "size");
+        assertCloseEnough(list.sum(), expected.sum, "sum");
+        assertCloseEnough(list.average(), expected.average, "ave");
     });
 
     function assertCloseEnough(actual, expected) {
