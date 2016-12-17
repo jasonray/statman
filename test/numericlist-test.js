b1f523fdcdbc83d5d5446ad8fc4d205fd095d846
Jason Ray <jasonray@jayray.net>
2016-12-17 12:29:06 -0500

    create pending tests for numeric list

--- test/numericlist-test.js
+++ test/numericlist-test.js
@@ -11,5 +11,22 @@ describe('numeric list', function () {
         assert.ok(list);
     });
 
+    it('given an empty list, sum should return 0')
+    it('given a list of one integer number, sum should return the number')
+    it('given a list of two integer numbers, sum should return the sum of the numbers')
+    it('given a list of five integer numbers, sum should return the sum of the numbers')
+
+    it('given a list of one decimal number, sum should return the number')
+    it('given a list of two decimal numbers, sum should return the sum of the numbers')
+    it('given a list of five decimal numbers, sum should return the sum of the numbers')
+
+    it('given an empty list, average should return 0')
+    it('given a list of one integer number, average should return the number')
+    it('given a list of two integer numbers, sum should return the average of the numbers')
+    it('given a list of five integer numbers, sum should return the average of the numbers')
+
+    it('given a list of one decimal number, average should return the number')
+    it('given a list of two decimal numbers, sum should return the average of the numbers')
+    it('given a list of five decimal numbers, sum should return the average of the numbers')
 
 });
