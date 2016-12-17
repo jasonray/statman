b59f36773dc8cbbef16ddc474331ccc47e1bc387
Jason Ray <jasonray@jayray.net>
2016-12-17 12:35:30 -0500

    numeric list: implement sum

--- test/numericlist-test.js
+++ test/numericlist-test.js
@@ -11,7 +11,11 @@ describe('numeric list', function () {
         assert.ok(list);
     });
 
-    it('given an empty list, sum should return 0')
+    it('given an empty list, sum should return 0', function () {
+        var list = new NumericList();
+        assert.equal(list.sum(), 0);
+    });
+
     it('given a list of one integer number, sum should return the number')
     it('given a list of two integer numbers, sum should return the sum of the numbers')
     it('given a list of five integer numbers, sum should return the sum of the numbers')
