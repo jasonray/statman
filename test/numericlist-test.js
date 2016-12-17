38caee00160bf03f9b08b1a3e92d8309b127f9ab
Jason Ray <jasonray@jayray.net>
2016-12-17 12:43:18 -0500

    numeric list: add sum test

--- test/numericlist-test.js
+++ test/numericlist-test.js
@@ -24,12 +24,20 @@ describe('numeric list', function () {
 
     it('given a list of two integer numbers, sum should return the sum of the numbers', function () {
         var list = new NumericList();
-        list.push(5);
-        list.push(10);
-        assert.equal(list.sum(), 15);
+        list.push(2);
+        list.push(4);
+        assert.equal(list.sum(), 6);
     });
 
-    it('given a list of five integer numbers, sum should return the sum of the numbers')
+    it('given a list of five integer numbers, sum should return the sum of the numbers', function () {
+        var list = new NumericList();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+        list.push(5);
+        assert.equal(list.sum(), 15);
+    });
 
     it('given a list of one decimal number, sum should return the number')
     it('given a list of two decimal numbers, sum should return the sum of the numbers')
