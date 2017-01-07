cd5e53f502c8ea0cbb1b03bc0b375be04fba484b
Jason Ray <jasonray@jayray.net>
2017-01-06 20:56:31 -0500

    working through statman registry

--- test/gauge-registry-test.js
+++ test/gauge-registry-test.js
@@ -2,10 +2,10 @@ var statman = require('../lib/statman');
 var assert = require('assert');
 var should = require('should');
 
-describe('registry', function () {
+describe.only('registry', function () {
 
     it('get new instance of a gauge', function () {
-        var gauge =  statman.gauge('metric-name');
+        var gauge = statman.gauge('metric-name');
         gauge.name().should.equal('metric-name');
     });
 
@@ -14,7 +14,7 @@ describe('registry', function () {
         statman.gauge('metric-name').value().should.equal(5);
     });
 
-    it.skip('two gauges from registry', function () {
+    it('two gauges from registry', function () {
         statman.gauge('metric-name-1').set(50);
         statman.gauge('metric-name-2').set(5);
         statman.gauge('metric-name-1').increment();
@@ -23,5 +23,14 @@ describe('registry', function () {
         statman.gauge('metric-name-1').value().should.equal(51);
         statman.gauge('metric-name-2').value().should.equal(4);
     });
+
+    it('reset', function () {
+        statman.gauge('a').value().should.be.equal(0);
+        statman.gauge('a').set(10);
+        statman.gauge('a').value().should.be.equal(10);
+        statman.reset();
+        statman.gauge('a').value().should.be.equal(0);
+    });
+
 
 });
