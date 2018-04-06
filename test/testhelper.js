6c9fe21dcaf1eb6651496280a5ebbd840103e49c
Jason Ray <jasonray@jayray.net>
2018-04-05 22:15:13 -0400

    move isNull from underscore to lodash

--- test/testhelper.js
+++ test/testhelper.js
@@ -1,9 +1,9 @@
-var _ = require('underscore');
 var assert = require('assert');
+const isNull = require('lodash.isnull');
 
 
 function assertCloseEnough(actual, expected, acceptedVariance) {
-    if (_.isNull(acceptedVariance)) acceptedVariance = 10;
+    if (isNull(acceptedVariance)) acceptedVariance = 10;
     var lowerThreshold = expected - acceptedVariance;
     var upperThreshold = expected + acceptedVariance;
     var message = "Expected " + expected + " ± " + acceptedVariance + ", was " + actual + ".";
