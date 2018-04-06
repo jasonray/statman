4539829858201034dde38415b3f66d6d7ec6cd2b
Jason Ray <jasonray@jayray.net>
2018-04-05 22:23:07 -0400

    var/const cleanup

--- test/gauge-test.js
+++ test/gauge-test.js
@@ -4,7 +4,7 @@
 const mocha = require('mocha');
 const assert = require('assert');
 const should = require('should');
-var statman = require('../lib/statman');
+const statman = require('../lib/statman');
 
 describe('gauge (smoke test)', function () {
     it('gauagename', function () {
